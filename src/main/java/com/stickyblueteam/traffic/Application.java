c8415eac7af07181990a8ed102d14625ac0da5a7
Muhammad Zubair <mzubair01@gmail.com>
2018-12-05 13:35:01 -0500

    first commit

---
+++ src/main/java/com/stickyblueteam/traffic/Application.java
@@ -0,0 +1,12 @@
+package com.stickyblueteam.traffic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
